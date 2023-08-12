632bb7835b64ad85158dbc511f6cfb95e219836e
Gabriel <gabrielmonteiroduete@gmail.com>
2023-08-11 23:53:40 -0300

    feat: organize initial structure

---
+++ src/components/SectionServices/SectionServices.spec.tsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react'
+
+import SectionServices from '.'
+
+describe('<SectionServices />', () => {
+  beforeEach(() => {
+    render(<SectionServices />)
+  })
+
+  it.todo('should render title and subtitle correctly')
+
+  it.todo('should render button correctly')
+
+  it.todo('should render services items correctly')
+})
---
+++ src/components/SectionServices/index.tsx
@@ -0,0 +1,12 @@
+const SectionServices = () => {
+  return (
+    <section>
+      <h1></h1>
+      <p></p>
+      <div></div>
+      <button />
+    </section>
+  )
+}
+
+export default SectionServices
