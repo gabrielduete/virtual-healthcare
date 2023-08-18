5ac6ec86ae06c02eee7d68fac1f0fc6f5490cbc1
Gabriel <gabrielmonteiroduete@gmail.com>
2023-08-17 23:33:57 -0300

    test: remove incorret prop

--- src/components/Title/Title.spec.tsx
+++ src/components/Title/Title.spec.tsx
@@ -6,7 +6,7 @@ describe('<Title />', () => {
   it('should render title correctly', () => {
     const title = 'Shiryu amigo'
 
-    render(<Title type='large'>{title}</Title>)
+    render(<Title>{title}</Title>)
 
     expect(screen.getByText(title)).toBeInTheDocument()
   })
